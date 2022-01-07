0ebd7984c7acdba4db78b4a40a226a3d6e383bba
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-07 20:25:13 +0800

    added the address of the static path to the root

--- qr_app/settings.py
+++ qr_app/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/clydeebennet/qr_app/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
