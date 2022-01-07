a904bce1c6a9cead53243b3123b6e2b16649d0bf
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-07 20:41:44 +0800

    configured mysql database

--- qr_app/settings.py
+++ qr_app/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'qr_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# modified the database to use mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clydeebennet$qr__app_db',
+        'USER': 'clydeebennet',
+        'PASSWORD': 'Wubbalubbadubdub291998',
+        'HOST': 'clydeebennet.mysql.pythonanywhere-services.com',
     }
 }
 
