d24397abeb5cd96179b5f89068e93069a35d2788
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 20:34:17 +0800

    added the view to allow users to register

--- qr/urls.py
+++ qr/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import qr_scanner_view
+from .views import qr_scanner_view, ContactCreateView
 
 urlpatterns = [
     path('index/', qr_scanner_view, name='qr_scanner'),
+    path('registration/', ContactCreateView.as_view(), name='registration')
 ]
