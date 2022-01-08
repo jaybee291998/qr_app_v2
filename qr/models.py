a348a4667641db4cb68aa15d80df341891c24784
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 18:45:43 +0800

    added the database model for the conatct information

--- qr/models.py
+++ qr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ContactInfomation(models.Model):
+	first_name 			= models.CharField(max_length=32)
+    last_name           = models.CharField(max_length=32)
+    address             = models.CharField(max_length=128)
+    phone               = models.CharField(max_length=11)
+    email               = models.CharField(max_length=64)
+    birthdate           = models.DateFiels()
+    qr_code             = models.CharField(max_length=6)
