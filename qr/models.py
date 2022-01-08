9a18b7711936ebe08cab2adb27b56abd74a0014e
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 19:02:48 +0800

    added the log records

--- qr/models.py
+++ qr/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.utils.transalation import gettext_lazy as _
 
 # Create your models here.
 class ContactInfomation(models.Model):
+    class Sex(modes.TextChoices):
+        MALE            = 'M', _('Male')
+        FEMALE          = 'F', _('Female')
+
 	first_name 			= models.CharField(max_length=32)
     last_name           = models.CharField(max_length=32)
+    sex                 = models.CharField(max_length=1, choices=Sex.choices, default=Sex.MALE)
     address             = models.CharField(max_length=128)
     phone               = models.CharField(max_length=11)
     email               = models.CharField(max_length=64)
@@ -12,3 +18,11 @@ class ContactInfomation(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+class Log(models.Model):
+    qr_code             = models.CharField(max_length=6)
+    temperature         = models.DecimalField(max_digits=3, decimal_places=1)
+    date                = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.qr_code} {self.temperature}: {self.date}'
