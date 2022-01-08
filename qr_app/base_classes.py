49f7cdd36c4f7464b0eb90ceecc242e8e91a0b71
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 19:30:14 +0800

    Create base_classes.py
    to store the base classes

---
+++ qr_app/base_classes.py
@@ -0,0 +1,18 @@
+import random
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+class BaseCreateView(CreateView):
+	def form_valid(self, form):
+		form.instance.qr_code = generate_qr_string()
+		form.save()
+
+		return super(BaseCreateView, self).form_valid(form)
+
+def generate_qr_string():
+	qr_str = ''
+	for _ in range(6):
+		qr_str += chr(radnom.randrange(32, 128, 1))
+
+	return qr_str
