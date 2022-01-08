896d6a6be51b5f1c8ad223233745679bd4bd4a22
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 19:06:55 +0800

    Update models.py

--- qr/models.py
+++ qr/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.transalation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class ContactInfomation(models.Model):
