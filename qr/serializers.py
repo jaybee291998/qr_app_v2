44766459c750b71dbb876ba0e300ecfadc7df6df
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-08 23:00:49 +0800

    Create serializers.py

---
+++ qr/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import ContactInformation
+
+class ContactSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = ContactInformation
+		fields = [
+			'first_name',
+			'last_name',
+			'sex',
+			'address',
+			'phone',
+			'email',
+			'birthdate'
+		]
