b9b28f0fbace079b1444c132818cbc40c0e8e3a5
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-09 19:18:29 +0800

    Update urls.py

--- qr/urls.py
+++ qr/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('registration/', ContactCreateView.as_view(), name='registration'),
     path('get_qr_code', v.get_qr_code_view, name='get_qr_code'),
     path('get_qr_code_api/', v.get_qr_code, name='get_qr_code_api'),
-    path('log_list_api/', v.LogList.as_view(), name='log_list_api')
+    path('log_list_api/', v.LogList.as_view(), name='log_list_api'),
+    path('get_log_list_api/', v.get_log_list_api, name='get_log_list_api'),
+    path('get_log_list/', v.get_log_list_view, name='get_log_list')
 ]
