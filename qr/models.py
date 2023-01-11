b7b0874b5f9662a06713886b1c91d9ebd36d0f22
jayvee-pearlpay <jaybee291998@gmail.com>
2023-01-11 09:36:18 +0800

    Update models.py

--- qr/models.py
+++ qr/models.py
@@ -29,8 +29,8 @@ class Log(models.Model):
 
 class QRRequest(models.Model):
     contact         = models.ForeignKey(ContactInformation, related_name="qr_request", on_delete=models.CASCADE, null=True)
-    latitude        = models.DecimalField(max_digits=20, decimal_places=10)
-    longitude       = models.DecimalField(max_digits=20, decimal_places=10)
+    latitude        = models.DecimalField(max_digits=50, decimal_places=40)
+    longitude       = models.DecimalField(max_digits=50, decimal_places=40)
     timestamp       = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
