e6e7045f60ac64eba8e030b8e3208fa624d4aead
jayvee-pearlpay <jaybee291998@gmail.com>
2023-03-02 19:09:22 +0800

    Update views.py

--- smssync/views.py
+++ smssync/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
-
+@api_view(['GET',])
 def task(request):
 	if request.method == 'GET':
 		task = request.GET.get('task')
